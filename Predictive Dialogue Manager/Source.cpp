5133973d0cb84460f50960de1179f690d3539615
Sean Kearns <c00194323@gmail.com>
2017-11-14 09:42:40 +0000

    First Commit

---
+++ Predictive Dialogue Manager/Source.cpp
@@ -0,0 +1,32 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+	IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG);
+	SDL_Window* window = SDL_CreateWindow("Predictive Dialogue Manager", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 800, SDL_WINDOW_SHOWN);
+	SDL_Texture *image = nullptr;
+
+	bool isRunning = true;
+
+	while (isRunning)
+	{
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				isRunning = false;
+			}
+	
+		}
+
+	}
+	SDL_Quit();
+	return 0;
+}
